57a2ea2dcf30a59db4dd4326bd1318ec05f4264d
anubhavprabhakar <anu.devjob@gmail.com>
2023-12-14 15:25:18 +0530

    add js file for game logic

---
+++ game.js
@@ -0,0 +1,49 @@
+function getComputerChoice(){
+    let x = Math.floor((Math.random() * 3) + 1);
+    if(x===1){
+        return "rock"
+    }else if(x===2){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+}
+
+function game(){
+    function playRound(playerSelection, computerSelection) {
+        let p=playerSelection.toLowerCase()
+        if(p==="rock"){
+            if(computerSelection==="paper"){
+                return "you lose. paper beats rock."
+            }else if(computerSelection==="scissors"){
+                return "you win! Rock beats scissors."
+            }else{
+                return "draw. play again"+" choice: "+p
+            }
+        }else if(p==="paper"){
+            if(computerSelection==="rock"){
+                return "you win! paper beats rock."
+            }else if(computerSelection==="scissors"){
+                return "you lose. scissors beat paper."
+            }else{
+                return "draw. play again"+" choice: "+p
+            }
+        }else if(p==="scissors"){
+            if(computerSelection==="rock"){
+                return "you lose! rock beats scissors."
+            }else if(computerSelection==="paper"){
+                return "you win. scissors beat paper."
+            }else{
+                return "draw. play again"+" choice: "+p
+            }
+        }
+    }
+    
+    for(let i=0; i<5; i++){
+        const playerSelection = prompt('Choose from rock paper or scissors:');
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game()
