b15fdd4ee292761880c357842946fc63932e6a78
anubhavprabhakar <anu.devjob@gmail.com>
2024-02-03 00:37:52 +0530

    add game logic

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+const rps = {'rock':1, 'paper':2, 'scissors':3}
+const wins = {'-2':'Player', '-1': 'Computer', '1':'Player', '2':'Computer'}
+
+const computersChoice = document.querySelector('.computersChoice')
+const playersChoice = document.querySelector('.playersChoice')
+const gameresult = document.querySelector('.gameresult')
+
 function getComputerChoice(){
     let x = Math.floor((Math.random() * 3) + 1);
     if(x===1){
@@ -10,35 +17,25 @@ function getComputerChoice(){
 }
 
 function game(){
-    function playRound(playerSelection, computerSelection) {
-        let p=playerSelection.toLowerCase()
-        if(p==="rock"){
-            if(computerSelection==="paper"){
-                return "you lose. paper beats rock."
-            }else if(computerSelection==="scissors"){
-                return "you win! Rock beats scissors."
-            }else{
-                return "draw. play again"+" choice: "+p
-            }
-        }else if(p==="paper"){
-            if(computerSelection==="rock"){
-                return "you win! paper beats rock."
-            }else if(computerSelection==="scissors"){
-                return "you lose. scissors beat paper."
-            }else{
-                return "draw. play again"+" choice: "+p
-            }
-        }else if(p==="scissors"){
-            if(computerSelection==="rock"){
-                return "you lose! rock beats scissors."
-            }else if(computerSelection==="paper"){
-                return "you win. scissors beat paper."
-            }else{
-                return "draw. play again"+" choice: "+p
-            }
+    function playRound(event) {
+        const playerSelection = event.target.id
+        playersChoice.textContent = playerSelection
+
+        const computerSelection = getComputerChoice()
+        computersChoice.textContent = computerSelection
+
+        const result = (rps[playerSelection]-rps[computerSelection])
+        
+        if(result === 0){
+            gameresult.textContent = 'draw'
+        }else{
+            gameresult.textContent = `${wins[result]} wins!`
         }
     }
-    
+    const choice = document.querySelector('.choice')
+    choice.addEventListener('click', playRound)
 }
+
+
 
 game()
